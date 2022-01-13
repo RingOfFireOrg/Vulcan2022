4c6246192de89c9ffae4eb4d72175871e4be4395
AdiPrk <33582614+AdiPrk@users.noreply.github.com>
2022-01-12 20:39:05 -0500

    Update Robot.java

--- Vulcan2022/src/main/java/frc/robot/Robot.java
+++ Vulcan2022/src/main/java/frc/robot/Robot.java
@@ -4,38 +4,49 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
+  //private DifferentialDrive m_myRobot;
   private Joystick m_leftStick;
-  private Joystick m_rightStick;
+  //private Joystick m_rightStick;
 
   private final MotorController m_leftMotor = new PWMSparkMax(0);
-  private final MotorController m_rightMotor = new PWMSparkMax(1);
+  //private final MotorController m_rightMotor = new PWMSparkMax(1);
+
+  private final VictorSP jeremyMotor = new VictorSP(2);
+  private final VictorSP jeremyMotor2 = new VictorSP(3);
 
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    
+    //m_rightMotor.setInverted(true);
 
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+    //m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
     m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    //m_rightStick = new Joystick(1);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    jeremyMotor.set(m_leftStick.getY());
+    jeremyMotor2.set(-m_leftStick.getY());
+
+    //m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
   }
 }
