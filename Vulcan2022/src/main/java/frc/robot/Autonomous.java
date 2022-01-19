4488f089813ecdbdc9e51cd441acd5901b5f6e4b
Adityaprk <Adityaprk04@gmail.com>
2022-01-19 18:32:38 -0500

    some imports

--- Vulcan2022/src/main/java/frc/robot/Autonomous.java
+++ Vulcan2022/src/main/java/frc/robot/Autonomous.java
@@ -3,17 +3,19 @@ package frc.robot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  
 import javax.swing.TransferHandler.TransferSupport;
-  
+
+import com.revrobotics.RelativeEncoder;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.SerialPort;
 
-//NEED IMPORTS
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Autonomous {
-    //private SpeedControllerGroup leftMotors, rightMotors; 
-    //private CANEncoder leftEncoder, rightEncoder;
+    private MotorControllerGroup leftMotors, rightMotors; 
+    private RelativeEncoder leftEncoder, rightEncoder;
     private int autonomousStep = -1;
     private double FEET = 8.50; // To go one FEET, the robot encoder has to read ~8.50 inches of the wheel
     
