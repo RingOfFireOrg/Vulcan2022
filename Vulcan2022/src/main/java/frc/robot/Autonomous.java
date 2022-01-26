9ab650d1352e2a95ebea20a1b9072815b7b91a9a
Adityaprk <Adityaprk04@gmail.com>
2022-01-25 20:41:13 -0500

    ya

--- Vulcan2022/src/main/java/frc/robot/Autonomous.java
+++ Vulcan2022/src/main/java/frc/robot/Autonomous.java
@@ -18,10 +18,63 @@ public class Autonomous {
     private RelativeEncoder leftEncoder, rightEncoder;
     private int autonomousStep = -1;
     private double FEET = 8.50; // To go one FEET, the robot encoder has to read ~8.50 inches of the wheel
+    private double leftDiff = 0, rightDiff = leftDiff;
+    private double lastLeftDiff = 0, lastRightDiff = lastLeftDiff;
+    /**
+     * STEPS to 4 ball auto + 1 human player
+     * 
+     * 1. Shoot preloaded ball
+     * 2. Turn to ball with vision (approx 180 - x)
+     * 3. Intake on
+     * 4. Forward 42" (best if distance is caluclated with vision)
+     * (ball picked up)
+     * 5. Turn to ball with vision (approx 147.75°)
+     * 6. Forward 120" (best if distance is calculated with vision)
+     * (ball picked up)
+     * 7. Turn to goal with vision (approx 120°)
+     * 8. Shoot two balls into goal
+     * 9. Turn to final ball (approx 170°) (at human player station)
+     * 10. Forward 160" (Likely won't work, vision plz!!!!!)
+     * (ball picked up)
+     * 11. Backward the distance moved last step
+     * 12. Aim to goal with vision
+     * 13. Shoot
+     * 
+     * Human: Aimbot
+     */
     
-    public void autonomousInit() {
-        
+    public double getLeftEncoderDistance() {
+        return Container.getInstance().getLeftInches();
     }
+    public double getRightEncoderDistance() {
+        return Container.getInstance().getRightInches();
+    }
+
+    public void calculateEncoderDiff() {
+        leftDiff = getLeftEncoderDistance() - lastLeftDiff;
+        rightDiff = getRightEncoderDistance() - lastRightDiff;
+
+        lastLeftDiff = leftDiff;
+        lastRightDiff = rightDiff;
+    }
+
+    public void move(String direction, double leftSpeed, double rightSpeed) {
+        double offset = 0.05;
+
+        calculateEncoderDiff();
+        
+        if (leftDiff > rightDiff) {
+            leftSpeed -= offset;
+            rightSpeed += offset;
+        } else if (leftDiff < rightDiff) {
+            leftSpeed += offset;
+            rightSpeed -= offset;
+        }
+
+        leftMotors.set(leftSpeed);
+        rightMotors.set(rightSpeed);
+    }
+
     /*
     switch (autonomousStep) {
         case 0: {
